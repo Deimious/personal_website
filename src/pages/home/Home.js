915c4802f61cd043a5dbbda1ce88a27b3bcd4d6c
Deimious <baalnaroz@tutanota.com>
2022-11-03 23:03:51 -0500

    adding in 'loading screen'

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,14 +1,18 @@
-import logo from '../../resources/images/logo.svg';
 import '../../style.css';
-import TitleBar from './components/TitleBar.js'
+import Cog from '../../resources/images/cog_color.png'
+// import TitleBar from './components/TitleBar.js'
 
 function Home() {
   return (
-    <div className="Home">
-      <header className="Home-header">
-        <TitleBar name="Dylan"></TitleBar>
-        <img src={logo} className="Home-logo" alt="logo" />
-      </header>
+    <div className="home--container">
+      <div className="direction-box">
+          <img 
+            className="maint-icon"
+            src={Cog} 
+            alt="cog_icon"
+          />
+        <p>I'm still working on this, try again later.</p>
+      </div>
     </div>
   );
 }
